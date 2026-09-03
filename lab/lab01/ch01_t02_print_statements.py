c35718568c6934fa8ee9022b8442becb3764b482
benzson0123 <benzsonhkdi@gmail.com>
2026-09-03 09:15:37 +0000

    [skip actions] lab/lab01/ch01_t02_print_statements.py

--- lab/lab01/ch01_t02_print_statements.py
+++ lab/lab01/ch01_t02_print_statements.py
@@ -1,3 +1,3 @@
 print("Hello World!")
 print("Deep into distant woodlands winds a mazy way, reaching to overlapping spurs of mountains bathed in their hill-side blue.")
-prints "Hello [your_name]"
+prints "Hello [your_name"
