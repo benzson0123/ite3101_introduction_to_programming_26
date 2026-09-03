8136565fad00d34d3ce08035f85e7b0b30e4725e
benzson0123 <benzsonhkdi@gmail.com>
2026-09-03 08:47:38 +0000

    [skip actions] lab/lab01/ch01_t02_print_statements.py

--- lab/lab01/ch01_t02_print_statements.py
+++ lab/lab01/ch01_t02_print_statements.py
@@ -1,1 +1,2 @@
-print ("statements.py")
+print "Hello World!" #Python2
+print("Deep into distant woodlands winds a mazy way, reaching to overlapping spurs of mountains bathed in their hill-side blue.") 
