13693510ca0fd1cf3ef9f66785c98830a6729dfa
benzson0123 <benzsonhkdi@gmail.com>
2026-09-17 08:26:55 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -4,8 +4,7 @@ caesar = "Graham"
 praline = "John"
 viking = "Teresa"
 
-
 # Put your variables above this line, and uncomment the following 3 line of codes.
-# print(caesar)
-# print(praline)
-# print(viking)
+ print(caesar)
+ print(praline)
+ print(viking)
