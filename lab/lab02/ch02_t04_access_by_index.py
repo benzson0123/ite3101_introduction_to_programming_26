f6a0d59d6e0bae235482ff2b61a3e251a3efcd52
benzson0123 <benzsonhkdi@gmail.com>
2026-09-17 08:40:05 +0000

    [skip actions] lab/lab02/ch02_t04_access_by_index.py

--- lab/lab02/ch02_t04_access_by_index.py
+++ lab/lab02/ch02_t04_access_by_index.py
@@ -10,6 +10,6 @@ numbered 0 to 5, as shown below:
 So if you wanted "Y", you could just type
 "PYTHON"[1] (always start counting from 0!)
 """
-fifth_letter = "MONITY"
+fifth_letter = "MONITY"[4]
 
 print(fifth_letter)
