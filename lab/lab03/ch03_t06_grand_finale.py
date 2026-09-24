2e2dba0fbd60771640f85dfaedb207df4ed98ebb
benzson0123 <benzsonhkdi@gmail.com>
2026-09-24 08:40:41 +0000

    [skip actions] lab/lab03/ch03_t06_grand_finale.py

--- lab/lab03/ch03_t06_grand_finale.py
+++ lab/lab03/ch03_t06_grand_finale.py
@@ -3,4 +3,4 @@ from datetime import datetime
 now = datetime.now()
 
 print('%02d:%02d:%04d %02d:%02d:%02d' %
-      (now.mouth, now day, now.year, now.hour, now.minute, now.second))
+      (now.mouth, now.day, now.year, now.hour, now.minute, now.second))
